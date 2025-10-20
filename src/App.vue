bbc5f93d416b86c969dfcadbde2534d0a310fe27
CC4444443 <cc4444443@example.com>
2025-10-20 15:05:33 +0800

    3.2

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,22 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <nav>
+    <a href="#">Home</a> |
+    <a href="#">About</a>
+  </nav>
+  <div class="home">
+    <HelloWorld />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+nav {
+  padding: 1rem;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+nav a {
+  margin: 0 0.5rem;
+  color: inherit;
+  text-decoration: none;
 }
 </style>

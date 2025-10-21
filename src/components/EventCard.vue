0ec970fc2ad4e89f101eb86519802c4a00e65f9a
CC4444443 <cc4444443@example.com>
2025-10-21 16:40:14 +0800

    EventCard: accept props; HomeView: render multiple events; add types

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
-// defineProps<{
-//   msg: string
-// }>()
+import type { Event } from '../types'
+
+defineProps<{
+  event: Event
+}>()
 </script>
 
 <template>
-  <div class="greetings">
+  <div class="event-class">
     <div class="event-card">
-      <h2>EventCard placeholder</h2>
-      <p>This is a placeholder for the event card component.</p>
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
   </div>
 </template>
 
 <style scoped>
-.event-card { padding: 1rem; border: 1px solid #eaeaea; max-width: 600px; margin: 1rem auto }
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
 </style>

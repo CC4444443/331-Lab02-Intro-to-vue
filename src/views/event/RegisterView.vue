d28376e6d36e0ca1e3ffc4b68b731dd3ccbeb3bf
CC4444443 <cc4444443@example.com>
2025-10-24 11:48:51 +0800

    feat(event): add register button and programmatic navigation in RegisterView

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
+import { useRouter } from 'vue-router'
 import { type Event } from '../../types'
 
 const props = defineProps<{
@@ -8,8 +9,18 @@ const props = defineProps<{
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
+
+const router = useRouter()
+
+const register = async () => {
+  // TODO: call real registration API when available
+  console.log('[RegisterView] register clicked for event', event?.value?.id)
+  // simulate successful registration and navigate back to detail
+  router.push({ name: 'event-detail-view' })
+}
 </script>
 
 <template>
   <p>Register event here</p>
+  <button @click="register">Register</button>
 </template>

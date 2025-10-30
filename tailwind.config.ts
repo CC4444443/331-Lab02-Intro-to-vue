9b902e4e17a50c5a9a6c07825c0ebdec801e3a01
CC4444443 <cc4444443@example.com>
2025-10-30 14:35:33 +0800

    Add TailwindCSS setup: tailwind.config.ts, assets/style.css, update postcss and import in main.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+// https://tailwindcss.com/docs/configuration
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
